87cd7ae626a15c1962701008ebc02f5d649ac29f
evan stein <evanstein28@gmail.com>
2020-03-02 20:04:12 -0500

    adding cat_controller.js

---
+++ controllers/cats_controller.js
@@ -0,0 +1,71 @@
+const connection = require('../config/connection');
+
+//get all cats
+
+const getCats = () => {
+    return new Promise((reslove, reject) => {
+        connection.query('SELECT * FROM cats', (err, catdata) => {
+            if (err) {
+                consol.log(err);
+                //this goes to promise's .catch()
+                return reject(err);
+            }
+            //this goes to promise's .then()
+            reslove(catdata);
+        });
+    });
+};
+
+
+
+//create a cat
+/*accepts object paramater => {cat_name: "Mr. Mustaphlease"} */
+const createCat = (catObj) => {
+    return new promise((resolve, reject) => {
+
+        connection, query('INSERT INTO cats SET ?', catObj, (err, catdata) => {
+            if (err) {
+                consol.log(err);
+                //this goes to promise's .catch()
+                return reject(err);
+            }
+            //this goes to promise's .then()
+            reslove(catdata);
+        });
+    });
+};
+
+
+//update cats adoption status
+//catObj => {adopted: true} OR {adopted: false}
+const updateCat = (catObj, catId) => {
+    return new Promise((resolve, reject) => {
+
+        connection.query('UPDATE cats SET ? WHERE id = ?'[catObj, catId], (err, catdata) => {
+            if (err) {
+                console.log(err);
+                return reject(err);
+            } else if (catdata.affectedRows === 0) {
+                return resolve({ message: "Sorry could'nt find that cat" });
+            }
+            reslove({ message: "Cat updated successfully" });
+        });
+    });
+};
+
+//delete a cat 
+const deleteCat = (catId) => {
+    return new Promise((resolve, reject) => {
+        connection.query('DELETE FROM cats WHERE id = ?', [catId], (err, catdata) => {
+            if (err) {
+                console.log(err);
+                return reject(err);
+            } else if (catdata.affectedRows === 0) {
+                return resolve({ message: "Sorry could'nt find that cat" });
+            }
+            reslove({ message: "Cat deleted successfully" });
+        });
+    });
+};
+
+module.exports = { getCats, createCat, updateCat, deleteCat};
